70198bc0eb080a6adac0f49507b4c38b37d74ebc
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2024-02-26 22:14:49 -0300

    adicionado animação na sessão de perguntas

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,22 @@
 
 
 const center = document.querySelector(".center");
+const perguntas = document.querySelectorAll(".question");
+
+perguntas.forEach((pergunta) => {
+  pergunta.addEventListener("click", () => {
+    const perguntaAtivaAtual = document.querySelector(".ativo");
+
+    pergunta.classList.toggle("ativo")
+
+    if (perguntaAtivaAtual) {
+      perguntaAtivaAtual.classList.remove("ativo");
+    }
+
+    // pergunta.classList.add("ativo");
+  });
+});
+
 
 center.addEventListener("click", () => {
   center.classList.toggle("selecionado");
